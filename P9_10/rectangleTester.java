2c08ca2d5c394c046df4c51174520a34d719ce06
tsalim <tsalim@R216-10.student.local>
2014-10-08 11:37:50 -0700

    Add tester main

--- P9_10/rectangleTester.java
+++ P9_10/rectangleTester.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
+/* Name : Tony Lim, Joseph Park
+ * Date : Oct 8, 2014
+ * Assignment : P9.10 and P9.11
+ * Description : Making rectangle
+ */
+
 
 public class rectangleTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please insert the height : ");
+		int rec_height = in.nextInt();
+		System.out.print("Please insert the width  : ");
+		int rec_width = in.nextInt();
 
+
+		System.out.println("Here's the area" +  );
 	}
 
 }
