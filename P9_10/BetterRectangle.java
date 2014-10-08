d2bcc956ffdd44e18a48ce1b03bdf0321b68fceb
tsalim <tsalim@R216-10.student.local>
2014-10-08 11:30:54 -0700

    Add main

--- P9_10/BetterRectangle.java
+++ P9_10/BetterRectangle.java
@@ -2,5 +2,17 @@ import java.awt.Rectangle;
 
 
 public class BetterRectangle extends Rectangle{
-
+	
+	public BetterRectangle(int width, int height) {
+		super(width,height);
+	}
+	
+	public double getPerimeter() {
+		return 2*super.height + 2*super.width;
+	}
+	
+	
+	public double getArea() {
+		return super.height * super.width;
+	}
 }
