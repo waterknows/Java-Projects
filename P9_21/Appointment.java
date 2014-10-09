1e8a2c12c580a5ffe0ec82f6c2a02e057d521cce
jpark24 <jpark24@R216-09.student.local>
2014-10-09 10:46:36 -0700

    Finished Appointment Class

--- P9_21/Appointment.java
+++ P9_21/Appointment.java
@@ -1,4 +1,41 @@
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Appointment {
-
+	private String description;
+	private Calendar AppCalendar;
+	
+	public Appointment(){
+		description = "";
+		AppCalendar = Calendar.getInstance();
+	}
+	
+	public Appointment(String newDescription, Date NewDate){
+		description = newDescription;
+		AppCalendar.setTime(NewDate);
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public String getDate(){
+		return AppCalendar.toString();
+	}
+	
+	public void setDescription(String newDescription){
+		description = newDescription;
+	}
+	
+	public void setDate(Date NewDate){
+		AppCalendar.setTime(NewDate);
+	}
+	
+	public boolean occursOn(int year, int month, int day){
+		Calendar Temp = null;
+		Temp.set(year,month,day);
+		return AppCalendar.compareTo(Temp) == 0;
+	}
+	
 }
