bc0ac4cfa6b81d339c04c1c3dd42d45ad143cbbf
Tony Lim <tsalim@R216-12.student.local>
2014-10-14 10:35:28 -0700

    Add ItemFood class

---
+++ P10_35/restaurant/ItemFood.java
@@ -0,0 +1,14 @@
+package Restaurant;
+
+public class ItemFood {
+
+	//no need get, set, just use this variable everywhere
+
+	public String foodName;
+	public Double foodPrice;
+	
+	public ItemFood(String itemName, double price) {
+		foodName = itemName;
+		foodPrice = price;
+	}
+}
