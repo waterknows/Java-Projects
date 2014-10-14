29f9212fa60bceae1c9f07f1657644feea7e913c
Tony Lim <tsalim@R216-12.student.local>
2014-10-14 10:24:26 -0700

    Setting up setup bill class and tester class

---
+++ P10_35/testRestaurant.java
@@ -0,0 +1,16 @@
+/* Name : Joseph Park, Tony Lim
+ * Date : Oct 14, 2014
+ * Assignment : P10.35
+ * Description : Making a restaurant bill calculator
+ */
+
+
+
+public class testRestaurant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
