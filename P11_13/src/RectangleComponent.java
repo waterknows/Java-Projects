9b899a84bccfde78a430b6e12c349a035e1b00bd
jpark24 <jpark24@R216-09.student.local>
2014-10-21 10:41:12 -0700

    asdfasdf

--- P11_13/src/RectangleComponent.java
+++ P11_13/src/RectangleComponent.java
@@ -1,8 +1,10 @@
 import java.awt.Graphics;
+
 import javax.swing.JComponent;
 /**
 This component displays a rectangle that can be moved.
  */
+@SuppressWarnings("serial")
 public class RectangleComponent extends JComponent
 {
 	private static final int RECTANGLE_WIDTH = 20;
@@ -25,8 +27,13 @@ Moves the rectangle by a given amount.
 	 */
 	public void moveRectangleBy(int dx, int dy)
 	{
+		if(xLeft + RECTANGLE_WIDTH == 300 || xLeft == 0)
+			dx *= -1;
+		if(yTop + RECTANGLE_HEIGHT == 400 || yTop == 0)
+			dy *= -1;
 		xLeft = xLeft + dx;
 		yTop = yTop + dy;
 		repaint();
 	}
+
 }
--- P11_13/src/RectangleFrame.java
+++ P11_13/src/RectangleFrame.java
@@ -1,19 +1,25 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.Timer;
 /**
 This frame contains a moving rectangle.
  */
+@SuppressWarnings("serial")
 public class RectangleFrame extends JFrame
 {
 	private static final int FRAME_WIDTH = 300;
 	private static final int FRAME_HEIGHT = 400;
 	private RectangleComponent scene;
+	private int xMod;
+	private int yMod;
 	class TimerListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
 		{
+			//Function to check 
+				
 			scene.moveRectangleBy(1, 1);
 		}
 	}
