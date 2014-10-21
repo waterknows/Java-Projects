a77d591852d943d5149c9d7386e4ef433d60ac3f
Tony Lim <atomictheorist@gmail.com>
2014-10-21 10:31:36 -0700

    Add main viewer

---
+++ P11_13/src/viewer.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class viewer {
+
+	public static void main(String[] args) {
+				JFrame rectFrame = new RectangleFrame();
+				rectFrame.setTitle("Restaurant Bill");
+				rectFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				rectFrame.setVisible(true);
+
+	}
+
+}
