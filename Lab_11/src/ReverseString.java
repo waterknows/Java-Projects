f8feb9a77cf0a6e24d187fafb11b15d21b0f24c8
jpark24 <jpark24@R216-09.student.local>
2014-10-23 10:22:14 -0700

    added recursive reverse function

--- Lab_11/src/ReverseString.java
+++ Lab_11/src/ReverseString.java
@@ -1,9 +1,21 @@
+import javax.swing.Timer;
+
 
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int DELAY = 1;
+		String text = "Hello!";
+		String reversed = recursiveReverse(text);
+		System.out.println("Original string: " + text);
+		System.out.println("Reversed string: " + reversed);
+//		Timer t = new Timer(DELAY,);
 	}
-
+	
+	public static String recursiveReverse(String text){
+		if(text.length() == 1)
+			return text;
+		else
+			return recursiveReverse(text.substring(1)) + text.substring(0,1);
+	}
 }
