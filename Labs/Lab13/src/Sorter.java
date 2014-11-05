a38e7d87bd62ae22f9f3a670b5ff44e87e7a05e5
Tony Lim <atomictheorist@gmail.com>
2014-11-05 00:27:05 -0800

    Renaming project

---
+++ Labs/Lab13/src/Sorter.java
@@ -0,0 +1,15 @@
+
+
+public class Sorter {
+
+    public static void swap(int[] array, int a, int b)
+    {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    public static void moveLeft() {
+
+    }
+}
