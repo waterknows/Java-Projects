06e4a0dc7bc45ede1b1bb15ab5dc2526f6fa1b61
tony-lim <atomictheorist@gmail.com>
2014-11-06 10:44:41 -0800

    asdfasdf

--- Labs/Lab13/src/sortDemo.java
+++ Labs/Lab13/src/sortDemo.java
@@ -58,28 +58,6 @@ public class sortDemo {
     	threWPartition(a, i, to);    	
     }
     
-    public static void sortOriginal(int[] array, int from, int to){
-    	if(from >= to)
-    		return;
-    	int p = partition(array,from,to);
-    	sortOriginal(array,from,p);
-    	sortOriginal(array,p+1,to);
-    }
-    
-    public static int partition(int[] array, int from, int to){
-    	int pivot = array[from];
-    	int i = from - 1, j = to + 1;
-    	while(i < j){
-    		i++; j--;
-    		while(array[i] < pivot) 
-    			i++;
-    		while(array[j] > pivot) 
-    			j--;
-    		if(i < j)
-    			swap(array,i,j);
-    	}
-    	return j;
-    }
     
     public static void swap(int[] array, int i, int j){
     	int temp = array[i];
